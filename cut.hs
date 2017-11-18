ad6626058d5a0792a96cf57914bdbb6bf4dd79be
maarten <maarten.j.jacobs@gmail.com>
2017-11-18 14:26:41 +0000

    Support case where row does not contain delimiter

--- cut.hs
+++ cut.hs
@@ -16,9 +16,14 @@ type Field = Int
 type Row = String
 type Column = String
 
+data CutError = ContainsNoDelimiter
+    deriving (Show, Eq)
+
 -- TODO: better way of retrieving fields?
-cut :: Delim -> [Field] -> Row -> [String]
-cut delim fields row = [columns !! field | field <- fields']
+cut :: Delim -> [Field] -> Row -> Either CutError [String]
+cut delim fields row
+    | length columns == 1 = Left ContainsNoDelimiter
+    | otherwise           = Right [columns !! field | field <- fields']
     where columns = splitOn delim row
           fields' = map (subtract 1) fields
 
