70c0c4ffb2f2487e9ca2f6f215d6bb1c8b842999
maarten <maarten.j.jacobs@gmail.com>
2017-11-19 20:13:11 +0000

    Create commandline command with options

--- cut.hs
+++ cut.hs
@@ -1,5 +1,9 @@
 import Data.List.Split (splitOn)
+import Data.List (intercalate)
+import Data.Semigroup ((<>))
+import Data.Char (ord)
 import Text.Parsec
+import qualified Options.Applicative as Options
 
 {-
 cut.hs imitates the UNIX cut tool.
@@ -9,20 +13,20 @@ cut -f|--fields <fields>
     -d|--delimiter <delim=TAB>
 -}
 
-type Delim = String
+type Delim = Char
 type Row = String
 type Column = String
 type Range = [Int]
 
-data CutError = ContainsNoDelimiter
+data CutError = ContainsNoDelimiter Row
     deriving (Show, Eq)
 
 -- TODO: better way of retrieving fields?
-cut :: Delim -> [Range] -> Row -> Either CutError [String]
+cut :: Delim -> [Range] -> Row -> Either CutError Row
 cut delim rs row
-    | length columns == 1 = Left ContainsNoDelimiter
-    | otherwise           = Right . concat $ map (extractFields columns) normalisedRanges
-    where columns = splitOn delim row
+    | length columns == 1 = Left $ ContainsNoDelimiter row
+    | otherwise           = Right . intercalate [delim] . concat $ map (extractFields columns) normalisedRanges
+    where columns = splitOn [delim] row
           columnCount = length columns
           normalisedRanges = map (limitRange columnCount . map (subtract 1)) rs
           extractFields cols range = [cols !! field | field <- range]
@@ -70,3 +74,41 @@ openStart = do start <- digits
 
 ranges :: Parsec String st [Range]
 ranges = range `sepBy` (char ',')
+
+data CutOptions = CutOptions
+    { file      :: String
+    , fields    :: String
+    , delimiter :: String }
+
+cutOptions :: Options.Parser CutOptions
+cutOptions = CutOptions
+            <$> Options.argument Options.str (Options.metavar "FILE")
+            <*> Options.strOption
+                (  Options.long "fields"
+                <> Options.short 'f'
+                <> Options.metavar "LIST"
+                <> Options.help "select only these fields")
+            <*> Options.strOption
+                (  Options.long "delimiter"
+                <> Options.short 'd'
+                <> Options.metavar "DELIM"
+                <> Options.value "\t"
+                <> Options.help "use DELIM instead of TAB for field delimiter")
+
+printCutResult :: Either CutError Row -> IO ()
+printCutResult (Left (ContainsNoDelimiter row)) = putStrLn row
+printCutResult (Right row)                      = putStrLn row
+
+executeCut :: CutOptions -> IO ()
+executeCut (CutOptions file fields (delim:[])) =
+    do content <- readFile file
+       let rows' = map (cut delim (translateFieldExpr fields)) $ lines content
+       mapM_ printCutResult rows'
+executeCut _ = putStrLn "No options provided!!!"
+
+main :: IO ()
+main = executeCut =<< Options.execParser opts
+    where opts = Options.info (cutOptions Options.<**> Options.helper)
+            (Options.fullDesc
+            <> Options.progDesc "Print selected parts of lines from each FILE to standard output."
+            <> Options.header "hello - a test for optparse-applicative")
