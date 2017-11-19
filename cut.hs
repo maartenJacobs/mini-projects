e8d46361605c1b200b93b3b60c3a00cc8b8611c0
maarten <maarten.j.jacobs@gmail.com>
2017-11-19 12:36:54 +0000

    Add naive parsing of field-list

--- cut.hs
+++ cut.hs
@@ -1,4 +1,5 @@
-import Data.List.Split
+import Data.List.Split (splitOn)
+import Text.Parsec
 
 {-
 cut.hs imitates the UNIX cut tool.
@@ -8,22 +9,64 @@ cut -f|--fields <fields>
     -d|--delimiter <delim=TAB>
 -}
 
--- type CutOp = (String -> [String])
-
 type Delim = String
-type Field = Int
 type Row = String
 type Column = String
+type Range = [Int]
 
 data CutError = ContainsNoDelimiter
     deriving (Show, Eq)
 
 -- TODO: better way of retrieving fields?
-cut :: Delim -> [Field] -> Row -> Either CutError [String]
-cut delim fields row
+cut :: Delim -> [Range] -> Row -> Either CutError [String]
+cut delim rs row
     | length columns == 1 = Left ContainsNoDelimiter
-    | otherwise           = Right [columns !! field | field <- fields']
+    | otherwise           = Right . concat $ map (extractFields columns) normalisedRanges
     where columns = splitOn delim row
-          fields' = map (subtract 1) fields
+          columnCount = length columns
+          normalisedRanges = map (limitRange columnCount . map (subtract 1)) rs
+          extractFields cols range = [cols !! field | field <- range]
 
--- translateFieldExpr :: String -> [Field]
+limitRange :: Int -> Range -> Range
+limitRange max = takeWhile (< max)
+
+-- Translates field expressions into the requested fields.
+-- Field expressions are 1 or more ranges separated by commas.
+-- A range is denoted by 2 numbers separated by a hyphen.
+-- A range can also be open on one end, but not both. An open range has
+-- one of the 2 boundaries unspecified.
+translateFieldExpr :: String -> [Range]
+translateFieldExpr expr = case parse ranges expr expr of
+                            (Left _)   -> error "invalid field list"
+                            (Right rs) -> rs
+
+range :: Parsec String st Range
+range = try closedRange <|> try halfOpenRange <|> onlyDigits
+
+digits :: Parsec String st Int
+digits = read <$> (many1 $ oneOf ['0'..'9'])
+
+onlyDigits :: Parsec String st Range
+onlyDigits = (: []) <$> digits
+
+closedRange :: Parsec String st Range
+closedRange = do start <- digits
+                 char '-'
+                 end <- digits
+                 return [start..end]
+
+halfOpenRange :: Parsec String st Range
+halfOpenRange = openStart <|> openEnd
+
+openEnd :: Parsec String st Range
+openEnd = do char '-'
+             end <- digits
+             return [1..end]
+
+openStart :: Parsec String st Range
+openStart = do start <- digits
+               char '-'
+               return [start..]
+
+ranges :: Parsec String st [Range]
+ranges = range `sepBy` (char ',')
