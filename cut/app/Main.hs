dad38e4148cb425733acead6729c8fbb0b08d6ad
maarten <maarten.j.jacobs@gmail.com>
2017-11-25 14:02:59 +0000

    Accept file or standard input

--- cut/app/Main.hs
+++ cut/app/Main.hs
@@ -2,6 +2,8 @@ module Main where
 
 import qualified Options.Applicative as Options
 import Data.Semigroup ((<>))
+import System.IO (Handle, hGetContents, openFile, IOMode(ReadMode), stdin)
+import Control.Applicative ((<|>))
 import qualified Cut.Parser as Parser
 import qualified Cut.Core as Cut
 
@@ -22,16 +24,25 @@ TODO:
 ```
 cut -f|--fields <fields>
     -d|--delimiter <delim=TAB>
+    [FILE|standard input]
 -}
 
+data Input = FileInput FilePath | StdInput
+
 data CutOptions = CutOptions
-                        { file      :: String
+                        { file      :: Input
                         , fields    :: String
                         , delimiter :: String }
 
+fileInput :: Options.Parser Input
+fileInput = FileInput <$> Options.argument Options.str (Options.metavar "FILE")
+
+stdInput :: Options.Parser Input
+stdInput = pure StdInput
+
 cutOptions :: Options.Parser CutOptions
 cutOptions = CutOptions
-        <$> Options.argument Options.str (Options.metavar "FILE")
+        <$> (fileInput <|> stdInput)
         <*> Options.strOption
             (  Options.long "fields"
             <> Options.short 'f'
@@ -49,11 +60,18 @@ printCutResult (Left (Cut.ContainsNoDelimiter row)) = putStrLn row
 printCutResult (Right row)                      = putStrLn row
 
 executeCut :: CutOptions -> IO ()
-executeCut (CutOptions file fields [delim]) =
-    do content <- readFile file
-       let rows' = map (Cut.cutFields delim (Parser.translateFieldExpr fields)) $ lines content
+executeCut (CutOptions input fields [delim]) =
+    do (closeOnEnd, handle) <- inputToHandle input
+       content <- hGetContents handle
+       let ranges = Parser.translateFieldExpr fields
+       let rows' = map (Cut.cutFields delim ranges) $ lines content
        mapM_ printCutResult rows'
 executeCut _ = putStrLn "No options provided!!!"
+
+inputToHandle :: Input -> IO (Bool, Handle)
+inputToHandle (FileInput fpath) = do handle <- openFile fpath ReadMode
+                                     return (True, handle)
+inputToHandle StdInput          = return (False, stdin)
 
 main :: IO ()
 main = executeCut =<< Options.execParser opts
